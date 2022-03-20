0f698aae0c112d8b2d95bf6f7641a5370e19cd60
zuramai <saugi.dev@gmail.com>
2022-03-20 09:24:14 +0700

    add: create scss structure

---
+++ playground/src/components/Sidebar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import IconAdd from '~icons/carbon/add-alt'
+import IconHome from '~icons/carbon/home'
+</script>
+
+<template>
+    <div class="sidebar">
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <router-link to="/">
+                        <icon-home/>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/">
+                        <icon-add/>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/layouts/sidebar";
+</style>
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,14 +1,33 @@
 import { fileURLToPath, URL } from 'url'
+import Icons from 'unplugin-icons/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Icons({ 
+      compiler: 'vue3',
+      autoInstall: true,
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/assets/scss/abstracts/_animations.scss" as *;
+          @use "@/assets/scss/abstracts/_variables.scss" as *;
+          @use "@/assets/scss/abstracts/_mixins.scss" as *;
+          @use "@/assets/scss/abstracts/_helpers.scss" as *;
+        `
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
