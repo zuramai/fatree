578324845e4a980f0d28a71b3cdeb6f0aa91c5a6
zuramai <saugi.dev@gmail.com>
2022-03-20 15:31:29 +0700

    add: basic button component

---
+++ playground/src/components/button/Button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps({
+    color: {
+        default: "primary",
+        type: String,
+    },
+    size: {
+        default: "md",
+        type: String,
+    },
+})
+
+let buttonClasses = {
+    "btn rounded-full flex inline-block": true,
+    ["btn--"+props.color]: true,
+    ["btn--"+props.size]: true
+}
+</script>
+<template>
+    <button :class="buttonClasses">
+        <slot name="icon" ></slot>
+        <span class="ml-2"><slot></slot></span>
+    </button>
+</template>
--- playground/src/views/HomeView.vue
+++ playground/src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import Card from "../components/card/Card.vue";
+import XCard from "../components/card/Card.vue";
+import XButton from "../components/button/Button.vue";
+import IconPlus from "~icons/carbon/add-alt"
 let templates = [0,0,0,0,0,0,0,0]
 </script>
 
@@ -9,14 +11,19 @@ let templates = [0,0,0,0,0,0,0,0]
       <h1 class="home__title mb-5 text-4xl">Create your own family tree, it's free!</h1>
       <p class="w-3/4 tracking-wide">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pulvinar congue rhoncus. Proin rhoncus justo porttitor tellus imperdiet, vitae tristique urna posuere. Sed eu interdum augue. </p>
 
-      <button class="btn btn-primary"></button>
+      <x-button class="mt-5 bg-gradient-to-r from-blue-500 to-indigo-500 shadow-lg hover:shadow-xl">
+        <template #icon>
+          <icon-plus class="align-middle"/>
+        </template>
+        Create new
+      </x-button>
 
       <section class="choose-template mt-16">
         <h2 class="text-xl mb-5">Choose your template:</h2>
 
         <div class="template-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
           <div v-for="template in templates">
-            <card title="Stunning Fam" description="Lorem ipsum dolor sit amet"/>          
+            <x-card title="Stunning Fam" description="Lorem ipsum dolor sit amet"/>          
           </div>
         </div>
       </section>
