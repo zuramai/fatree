9ad261b55974ffa8b63356fbe9ea60ac2173014f
zuramai <saugi.dev@gmail.com>
2022-03-21 16:59:24 +0700

    add: family list

---
+++ playground/src/components/family/FamilyRow.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const props = defineProps({
+    name: String,
+    image: String
+})
+</script>
+<template>
+    <div class="family-row | flex px-5 py-2 hover:bg-gray-100 hover:cursor-pointer">
+        <img :src="image" :alt="`Image for ${name}`" class="avatar avatar-sm | mr-2">
+        <span class="font-medium leading-7">{{ name }}</span>
+    </div>
+</template>
+<style lang="scss">
+</style>
--- playground/src/components/layouts/sidebar/SidebarCreate.vue
+++ playground/src/components/layouts/sidebar/SidebarCreate.vue
@@ -1,9 +1,33 @@
 <script lang="ts" setup>
 import Collapse from "@/components/collapse/Collapse.vue"
+import { ref } from "vue";
+import FamilyRow from "../../family/FamilyRow.vue";
+import JaneDoeImg from "@/assets/images/janedoe.jpg"
+
+
+let familyMembers = ref([
+    {
+        name: "Jane Doe",
+    },
+    {
+        name: "Jane Doe",
+    },
+    {
+        name: "Jane Doe",
+    },
+])
 </script>
 <template>
     <div class="sidebar sidebar--editor">
-    <Collapse title="Family"></collapse>
-    <Collapse title="Shape"></collapse>
+    <collapse title="Family">
+        <!-- Family lists goes here -->
+        <div class="family-members">
+            <FamilyRow v-for="member in familyMembers" 
+                :image="JaneDoeImg"
+                :name="member.name">
+            </FamilyRow>
+        </div>
+    </collapse>
+    <collapse title="Shape"></collapse>
     </div>
 </template>
