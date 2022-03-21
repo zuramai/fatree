b2577b628c79fe601bc570bf9b0a20c2e2095613
zuramai <saugi.dev@gmail.com>
2022-03-21 16:59:14 +0700

    add: collapse component

--- playground/src/components/collapse/Collapse.vue
+++ playground/src/components/collapse/Collapse.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
+import IconChevronDown from "~icons/carbon/chevron-down"
+import IconChevronUp from "~icons/carbon/chevron-up"
 import { ref } from 'vue';
 
 const props = defineProps({
     title: String,
 })
+const emit = defineEmits(["click"])
 
-let isOpen = ref(true)
+let isOpen = ref(false)
+
+function toggleCollapse() {
+    isOpen.value = !isOpen.value
+    emit("click")
+}
 
 </script>
 <template>
     <div class="collapse">
-        <div class="collapse__title" @click="$emit('click', isOpen)">
-            {{ title }}
+        <div class="collapse__title | flex justify-between items-center" @click="toggleCollapse">
+            <span>{{ title }}</span>
+            <component :is="isOpen ? IconChevronUp : IconChevronDown"/>
         </div>
-        <div class="collapse__content">
+        <div class="collapse__content" v-if="isOpen">
             <slot></slot>
         </div>
     </div>
